c327c4ca4729792d4d59fc72d73bd01f6a6cb110
Carneiro <paulacarneiro215@gmail.com>
2021-11-18 11:21:28 +0000

    Fix default option

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const ContactPicker = ({ contacts, onChange }) => {
   return (
     <select onChange={onChange} required>
-      <option key='default' value='' selected disabled>
+      <option key='-1' value='' selected disabled>
         Select a contact...
       </option>
       {contacts.map(contact => (
